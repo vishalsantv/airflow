945b06cea7e3c41a315092030c97ce978bbd0831
vishal <vishal@tumblr.com>
2018-05-13 10:18:52 -0400

    A stress test

--- dags/hello.py
+++ dags/hello.py
@@ -7,8 +7,8 @@ def print_hello():
     return 'Hello world!'
 
 dag = DAG('hello_world', description='Simple tutorial DAG',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2017, 3, 20), catchup=False)
+          schedule_interval='* * * * *',
+          start_date=datetime(2018, 4, 20), catchup=False)
 
 dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
 

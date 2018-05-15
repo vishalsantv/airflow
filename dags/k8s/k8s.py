088bc58b5a9bb6ed25067961421393be72affcf7
vishal <vishal@tumblr.com>
2018-05-15 19:51:44 -0400

    testing dummy k8s operator

--- dags/k8s/k8s.py
+++ dags/k8s/k8s.py
@@ -3,7 +3,7 @@ from airflow import DAG
 from airflow.operators.contrib.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.python_operator import PythonOperator
 
-dag = DAG('hello_world', description='Simple tutorial DAG',
+dag = DAG('hello_world_k8s', description='Simple tutorial DAG',
           schedule_interval='* * * * *',
           start_date=datetime(2018, 4, 20), catchup=False)
 

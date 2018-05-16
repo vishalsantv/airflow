938d0b19a0439a6bf1595477295979603a85694a
vishal <vishal@tumblr.com>
2018-05-15 20:35:03 -0400

    execute once

--- dags/k8s/k8s.py
+++ dags/k8s/k8s.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
+from datetime import timedelta
 
 dag = DAG('hello_world_k8s', description='Simple tutorial DAG',
           schedule_interval=timedelta(days=1)
